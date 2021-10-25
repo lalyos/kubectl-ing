8edc9d2ca4d985aa2982d9ec77257be2e2699233
lalyos <lalyos@yahoo.com>
2021-10-25 13:00:54 +0200

    updated SHA for v0.0.6

--- release.sh
+++ release.sh
@@ -11,6 +11,6 @@ git push origin HEAD --tags
 
 HASH=$(curl -sL https://github.com/lalyos/kubectl-ing/archive/${NEWVER}.tar.gz| shasum -a 256 | head -c 64)
 gsed -i "s/sha256:.*/sha256: $HASH/" plugins/ing.yaml
-gsed -i "s/$VER/$NEWVER/" plugins/ing.yaml
+gsed -i "s/${VER#v}/${NEWVER#v}/" plugins/ing.yaml
 git commit -am "updated SHA for $NEWVER"
 git push origin HEAD
